9bd5939572b5283477d1129888a004edb804c129
jiayangSun <jiayang@airspace.co>
2017-06-07 21:26:46 +0000

    convert grey and subtract

--- camtest/camtest.cpp
+++ camtest/camtest.cpp
@@ -4,46 +4,40 @@
 /*
 	A collection of different GST camera piplines
 	we tried.
-
 */
-
-
 
 using namespace cv;
 using namespace std;
 
-int main(int argc, char** argv )
-{
+int main(int argc, char** argv) {
 
-	VideoCapture cap(1); // open the camera
+    VideoCapture cap(1); // open the camera
 
 
-	if(!cap.isOpened()){ 
-		// check if we succeeded
-		cout << "Capture open failed !\n" << endl;
-		return -1;
+    if (!cap.isOpened()) { 
+	// check if we succeeded
+        cout << "Capture open failed !\n" << endl;
+	return -1;
+    }
+
+    namedWindow("edges",1);
+    Mat frameLast;
+    for (;;) {
+	Mat frameCurrent;
+	cap >> frameCurrent; // get a new frame from camera
+	if (frameCurrent.empty()) {
+	    break;
+        }
+	cvtColor(frameCurrent, frameCurrent, CV_BGR2GRAY);
+	if (!frameLast.empty()) {
+	    frameLast -= frameCurrent;
+	    imshow("edges", frameLast);
 	}
-
-	namedWindow("edges",1);
-	Mat frameLast;
-	for(;;)
-	{
-		Mat frameCurrent;
-		cap >> frameCurrent; // get a new frame from camera
-		if(frameCurrent.empty())
-			break;
-		if(!frameLast.empty())
-		{
-			Mat grey;
-			frameLast -= frameCurrent;
-			cvtColor(frameLast, grey, CV_BGR2GRAY);
-			imshow("edges", grey);
-		}
-		frameLast = frameCurrent;
-		if(waitKey(30) >= 0) break;
-	}
-	return 0;
-
-
+	frameLast = frameCurrent;
+	if (waitKey(30) >= 0) {
+            break;
+        }
+    }
+    return 0;
 }
 
